18b68b81b38cace326f3315d5a1b439f0bfac7cd
Thanakarn Klangkasame <thanakarn.klangkasame@gmail.com>
2024-10-21 16:10:07 +0700

    Add Token Field

--- Promrub.Services.API/Entities/PaymentTransactionEntity.cs
+++ Promrub.Services.API/Entities/PaymentTransactionEntity.cs
@@ -55,5 +55,8 @@ namespace Promrub.Services.API.Entities
         [Column("create_at")]
         public DateTime? CreateAt { get; set; }
 
+        [Column("auth_token")]
+        public string? Token { get; set; }
+
     }
 }
