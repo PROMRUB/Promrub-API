6f831c37ea67c88bec46d7b48cfe6dee61c47a4c
Thanakarn Klangkasame <thanakarn.klangkasame@gmail.com>
2024-11-09 15:02:56 +0700

    [Update] Payment Channel Controller

--- Promrub.Services.API/Interfaces/IPaymentChannelServices.cs
+++ Promrub.Services.API/Interfaces/IPaymentChannelServices.cs
@@ -1,9 +1,12 @@
-﻿using Promrub.Services.API.Models.RequestModels.Payment;
+﻿using Promrub.Services.API.Entities;
+using Promrub.Services.API.Models.RequestModels.Payment;
 
 namespace Promrub.Services.API.Interfaces
 {
     public interface IPaymentChannelServices
     {
+        public Task<List<PaymentChannelEntity>> GetPaymentChannel(string orgId);
         public void AddPaymentChannel(string orgId, PaymentChannelRequest request);
+        public Task UpdateBillerId(string orgId, Guid paymentChannelId, PaymentChannelRequest request);
     }
 }
